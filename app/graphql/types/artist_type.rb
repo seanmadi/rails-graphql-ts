43acf0f4fb045cb09f3179c933853a8313137923
Sean Madigan <sean.madigan@philips.com>
2023-01-23 19:32:35 +0000

    Add graphql and artist/item queries

---
+++ app/graphql/types/artist_type.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Types
+  class ArtistType < Types::BaseObject
+    field :id, ID, null: false
+    field :first_name, String, null: true
+    field :last_name, String, null: true
+    field :full_name, String, null: true
+    field :items, [Types::ItemType], null: false
+    field :email, String
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    def full_name
+      [object.first_name, object.last_name].compact.join(" ")
+    end
+  end
+end
